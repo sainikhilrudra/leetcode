fd2cbc502148d2f761135e052e9b2aadc5c44f86
sainikhilrudra <134760488+sainikhilrudra@users.noreply.github.com>
2024-01-01 20:30:03 +0530

    Time: 2571 ms (33.33%) | Memory: 47 MB (100.00%) - LeetSync

--- 3266-find-longest-special-substring-that-occurs-thrice-ii/find-longest-special-substring-that-occurs-thrice-ii.java
+++ 3266-find-longest-special-substring-that-occurs-thrice-ii/find-longest-special-substring-that-occurs-thrice-ii.java
@@ -1,49 +1,34 @@
 class Solution {
 
-    static boolean isMidValid(char[] s, int n, int k) {
-        for (char ch = 'a'; ch <= 'z'; ch++) {
-            int i = 0, j = 0, cnt = 0, f = 0;
-
-            while (j < n) {
-                if (s[j] == ch) {
-                    cnt++;
-                }
-
-                if (j - i + 1 < k) {
-                    j++;
-                } else {
-                    if (cnt == k) {
-                        f++;
-                    }
-
-                    if (f >= 3) {
-                        return true;
-                    }
-
-                    if (s[i] == ch) {
-                        cnt--;
-                    }
-
-                    i++;
-                    j++;
-                }
-            }
-        }
-        return false;
-    }
+     static boolean isMidValid(char[] s, int n, int k) {
+         for(char ch='a'; ch<='z'; ch++) {
+             int i=0, j=0, cnt=0, f=0;
+             while(j < n) {
+                 if(s[j] == ch) cnt++;
+                 if(j-i+1 < k) j++;
+                 else {
+                     if(cnt == k) f++;
+                     if(f >= 3) return true;
+                     if(s[i] == ch) cnt--;
+                     i++;
+                     j++;
+                 }
+             }
+         }
+         return false;
+     }
 
     public int maximumLength(String s) {
         int n = s.length();
         char[] ch = s.toCharArray();
-        int low = 1, high = n - 2, ans = -1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            
-            if (isMidValid(ch, n, mid)) {
+        int low = 1, high = n - 2, ans=-1;
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            if(isMidValid(ch, n, mid) == true) {
                 ans = mid;
                 low = mid + 1;
-            } else {
+            }
+            else {
                 high = mid - 1;
             }
         }
